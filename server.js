f8cd81a4723cb0c03e079335cabe6180f586ae2c
So Youn Choi <manghatuhyo@gmail.com>
2023-12-03 09:52:40 +0100

    do i need to import crypto? isnt it inside node js? i was just about to ask this but i need to check my other project is working or not, so i leave this here

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import crypto from 'crypto'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
