f01a9b7350876713cffd4e2bb70c4cc38402ff5e
So Youn Choi <manghatuhyo@gmail.com>
2023-12-03 14:58:19 +0100

    authentication codealong

--- server.js
+++ server.js
@@ -1,54 +1,47 @@
 import express from "express";
 import cors from "cors";
-import crypto from 'crypto'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.Promise = Promise
 
 const User = mongoose.model('User', {
-  name:{
+  name: {
+    type: String,
+    unique: true
+  }, 
+  email: {
     type: String,
     unique: true
   },
-  password:{
+  password: {
     type: String,
     required: true
   },
-  accessToken:{
-    type: String,
+  accessToken: {
+    type: String, 
     default: () => crypto.randomBytes(128).toString('hex')
   }
 })
 
-
-// one-way encryption
-const user = new User({name: "Bob", password: bcrypt.hashSync("foobar")})
-user.save()
-  .then((doc) => {
-    console.log("User saved successfully:", doc);
-  })
-  .catch((err) => {
-    console.error("Error saving user:", err);
-  })
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if(user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({loggedOut: true})
+  }
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-
-const authenticateUser = async ( req, res, next ) => {
-  const user = await User.findOne({accessToken: req.header('Authorization')})
-  if(user){
-    req.user = user;
-    next()
-  } else {
-    res.status(401).json({loggedOut: true})
-  }
-}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -59,21 +52,28 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-app.post('/tweets', authenticateUser)
-app.post('/tweets', async (req, res) => {
-  // This will only happen if the next() function is called from the middleware!
-  // now we can access the req.user object from the middleware
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email, password} = req.body
+    // DO NOT STORE PLAINTEXT PASSWORDS
+    const user = new User({name, email, password: bcrypt.hashSync(password)})
+    user.save()
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
+  } catch(err) {
+    res.status(400).json({message: 'Could not create user', errors: err.errors})
+  }
+})
+
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  res.json({secret: 'This is a super secret message.'})
 })
 
 app.post('/sessions', async (req, res) => {
-  const user = await User.findOne({name: req.body.name})
-  if(user && bcrypt.compareSync(req.body.password, user.password)){
-    // Success
+  const user = await User.findOne({email: req.body.email})
+  if(user && bcrypt.compareSync(req.body.password, user.password)) {
     res.json({userId: user._id, accessToken: user.accessToken})
   } else {
-    // Failure
-    // a. User does not exist
-    // b. Encrypted password does not match
     res.json({notFound: true})
   }
 })
@@ -82,5 +82,3 @@ app.post('/sessions', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-console.log(bcrypt.hashSync("foobar"))
